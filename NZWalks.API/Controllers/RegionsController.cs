cb70fe1a8d66d564fb65d060e0064ccc4a0a1c7c
SoongXianLim <soong-xian.lim@bassnet.no>
2025-08-03 16:57:38 +0800

    Create HttpGet for all record

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext nzWalksDbContext)
+        {
+            this.dbContext = nzWalksDbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllRegion()
+        {
+            var regions = dbContext.Regions.ToList();
+
+            return Ok(regions);
+        }
+    }
+}
