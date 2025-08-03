be19c65d9468f0da0f52c6230b3e68f51cd9c9cc
SoongXianLim <soong-xian.lim@bassnet.no>
2025-08-04 00:58:45 +0800

    Added repository pattern to Region controller

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using NZWalks.API.Data;
+using NZWalks.API.Repositories;
 
 namespace NZWalks.API
 {
@@ -22,6 +23,8 @@ namespace NZWalks.API
             });
 
             builder.Services.AddDbContext<NZWalksDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
+
+            builder.Services.AddScoped<IRegionRepository, RegionRepository>();
 
             var app = builder.Build();
 
