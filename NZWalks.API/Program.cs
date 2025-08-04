e44b4bcfc7d74accfdccbc2e0094a6e283da587c
SoongXianLim <soong-xian.lim@bassnet.no>
2025-08-04 09:59:50 +0800

    Updated Swagger title

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -19,7 +19,7 @@ namespace NZWalks.API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NZWalks.API", Version = "v1" });
             });
 
             builder.Services.AddDbContext<NZWalksDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
